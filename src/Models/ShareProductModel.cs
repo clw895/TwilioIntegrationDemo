56fd97b18abab3b20e54344dd0af6b6ee990cdde
Corey Weathers <clw895@msn.com>
2020-06-04 14:41:47 -0400

    Adds ShareProductModel to repo

---
+++ src/Models/ShareProductModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class ShareProductModel 
+    {
+        public string To {get;set;} = "";
+        public string Body {get;set;} = "";
+    }
+}
