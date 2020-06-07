a3ae5934eda8f29c4387bb823c8b10b8bbc8dac2
Corey Weathers <clw895@msn.com>
2020-06-06 21:49:53 -0400

    Adds completed Autopilot code

--- src/Controllers/AutopilotController.cs
+++ src/Controllers/AutopilotController.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using static ContosoCrafts.WebSite.Models.AutopilotResponse;
 
 namespace ContosoCrafts.WebSite.Controllers
@@ -29,7 +30,7 @@ namespace ContosoCrafts.WebSite.Controllers
                 x.Image,
                 x.Title,
                 x.Description
-            }).ToList();
+            }).Take(3).ToList();
 
             var response = new AutopilotResponse {
                Actions = new List<object>()
@@ -41,7 +42,7 @@ namespace ContosoCrafts.WebSite.Controllers
                 {
                     show = new Show
                     {
-                        Body = product.Description,
+                        Body = product.Title,
                         Images = new List<Show.Image>
                         {
                             {new Show.Image
@@ -57,9 +58,22 @@ namespace ContosoCrafts.WebSite.Controllers
         }
     
         [HttpPost("order")]
-        public IActionResult PlaceOrder(string order)
+        public IActionResult PlaceOrder([FromForm]string memory)
         {
-            return NoContent();
+            var json = JObject.Parse(memory);
+            var answers = json["twilio"]["collected_data"]["order_product"]["answers"];
+            var firstName = answers["first_name"]["answer"].ToString();
+            var productName = answers["product_name"]["answer"].ToString();
+            var product_count = answers["product_count"]["answer"].ToString();
+
+            var response = new {
+                actions = new[]{
+                    new { say = $"Thanks for your order {firstName}. We will send out an order of {product_count} {productName} shortly."}
+                }
+            };
+
+            return new JsonResult(response);
+
         }
     }
 }
